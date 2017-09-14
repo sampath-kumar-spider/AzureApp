7bdea648d3ff80f23fb09672a6795560565cf883
Sampath Kumar Chindirala <SChindirala@spiderlogic.com>
2017-09-14 10:40:46 +0530

    First checking to Gith Hub Repository.

---
+++ AzureAutoDeployment/AzureAutoDeployment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureAutoDeployment.Startup))]
+namespace AzureAutoDeployment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
